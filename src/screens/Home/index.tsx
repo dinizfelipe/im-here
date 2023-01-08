108823301d3300d479da92f9adb5bdddf8add9e8
Felipe Diniz <felipedinizdev@gmail.com>
2023-01-08 01:03:03 -0300

    Data Input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,17 @@
-import { Text, View } from 'react-native';
-import {styles} from './styles';
+import { Text, View, TextInput } from "react-native";
+import { styles } from "./styles";
 
-export default function Home(){
- return(
-  <View style={styles.container}>
-    <Text style={styles.eventName}>
-      Nome do evento
-    </Text>
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-    <Text style={styles.eventDate}>
-      Sexta, 07 de Janeiro de 2023
-    </Text>
-  </View>
-  )
+      <Text style={styles.eventDate}>Sexta, 07 de Janeiro de 2023</Text>
+      <TextInput 
+      style={styles.input} 
+      placeholder="Nome do participante"
+      placeholderTextColor="#6B6B6B"
+      />
+    </View>
+  );
 }
