f4ba883d82939f80ab4ef65c4494d5399541b035
Felipe Diniz <felipedinizdev@gmail.com>
2023-01-08 21:48:35 -0300

    Adicionando Participantes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   View,
@@ -6,36 +6,46 @@ import {
   TouchableOpacity,
   Alert,
   FlatList,
+  StatusBar,
 } from "react-native";
 import { Participant } from "../../components/Participant";
 
 import { styles } from "./styles";
 
 export function Home() {
-  const participants = [
-    "John",
-    "Mark",
-    "Peter",
-    "Dan",
-    "Well",
-    "Philip",
-    "Brian",
-    "Steven",
-    "Jack",
-    "Will",
-    "Smith",
-  ];
+  const [participants, setParticipants] = useState(["John"]);
 
   function handleParticipantAdd() {
-    Alert.alert("Weahoo!", "Você adicionou um participante.");
+    if (participants.includes("Mark")) {
+      return Alert.alert(
+        "Ooops!",
+        "Já existe um participante na lista com esse nome."
+      );
+    }
+    setParticipants((oldState) => [...oldState, "Ana"]);
   }
 
   function handleParticipantRemove(name: string) {
-    Alert.alert("Oops!", ` Você acabou de remover o paricipante ${name}.`);
+    Alert.alert("Opa!", ` Você deseja remover o participante ${name}?`, [
+      {
+        text: "Sim",
+        onPress: () =>
+          Alert.alert(
+            "Sucesso!",
+            `O participante ${name} foi removido da lista.`
+          ),
+      },
+      { text: "Não", style: "cancel" },
+    ]);
   }
 
   return (
     <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 07 de Janeiro de 2023</Text>
 
@@ -58,7 +68,7 @@ export function Home() {
           <Participant
             key={item}
             name={item}
-            onRemove={() => handleParticipantRemove("Felipe")}
+            onRemove={() => handleParticipantRemove(item)}
           />
         )}
         showsVerticalScrollIndicator={false}
